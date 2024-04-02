854c37a56e13955a22cba8a7bb746a482c49c67b
Nuno Silva <59452877+komen205@users.noreply.github.com>
2024-04-02 23:15:47 +0100

    Create InfiniteOxygen.cs

---
+++ InfiniteOxygen.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace InfiniteOxygenContentWarning
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class InfiniteOxygen : BaseUnityPlugin
+    {
+        private const string modGUID = "komen205.InfiniteOxygen";
+        private const string modName = "InfiniteOxygen";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private void Awake()
+        {
+            harmony.PatchAll();
+
+            Logger.LogInfo($"Plugin {modGUID} is loaded!");
+        }
+
+        [HarmonyPatch(typeof(Player.PlayerData))]
+        internal class PlayerDataPatch
+        {
+            [HarmonyPatch("UpdateValues")]
+            [HarmonyPrefix]
+            static void IsGameFullPatch(ref float ___remainingOxygen)
+            {
+                ___remainingOxygen = 500f;
+            }
+        }
+    }
+}
